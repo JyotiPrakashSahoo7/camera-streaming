cc8850707ed048ff2dc6d182bc20aa990fa850c9
Jyoti Prakash <sahoojyotiprakash2005@gmail.com>
2025-12-04 09:32:12 +0530

    fix: use Render PORT

--- server.js
+++ server.js
@@ -3,20 +3,31 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+
+// IMPORTANT: Allow CORS for Socket.io on Render
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
     console.log("A user connected: " + socket.id);
+    
     socket.on("camera-frame", (data) => {
         io.emit("stream", data);
     });
+
     socket.on("disconnect", () => {
         console.log("User disconnected: " + socket.id);
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+// FIXED: Render requires dynamic PORT
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
